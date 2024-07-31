43c0b572f61051621c224361e244ab57d5afab72
ijuntan <juniortanaya@gmail.com>
2024-07-31 22:04:28 +0800

    feat: add palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const newString = string.toLowerCase().split("").filter(s => s.match(/^[1-9a-zA-Z]$/)).join("")
+    for(i = 0; i < Math.floor(newString.length/2); i++) {
+        if(newString[i] !== newString.at(-1 - i)) return false
+    }
 
+    return true
 };
 
 // Do not edit below this line
