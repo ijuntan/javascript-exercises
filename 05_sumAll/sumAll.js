e29ea33c104f3fc05481ee07ab630e2829f0245a
ijuntan <juniortanaya@gmail.com>
2024-07-31 16:56:19 +0800

    feat: add sum all function with error validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if(
+        typeof numOne !== "number" || 
+        typeof numTwo !== "number" ||
+        numOne % 1 !== 0 ||
+        numTwo % 1 !== 0 ||
+        numOne < 0 || 
+        numTwo < 0
+    ) return "ERROR"
 
+    let large = numTwo > numOne ? numTwo : numOne
+    let odd = large % 2 === 0
+
+    if(odd) return (numOne + numTwo) * large / 2
+    else return (numOne + numTwo - 1) * (large - 1) / 2 + large
 };
 
 // Do not edit below this line
