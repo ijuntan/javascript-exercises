2825ea50acb3741df1eda1f716358032b9b460bd
ijuntan <juniortanaya@gmail.com>
2024-07-31 16:37:41 +0800

    feat: recursive remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const recursiveRemove = (tempArray, value) => {
+    const index = tempArray.findIndex(el => el === value)
+    if(index !== -1) {
+        tempArray.splice(index, 1)
+    }
+    const anotherIndex = tempArray.findIndex(el => el === value)
+    if(anotherIndex === -1) {
+        return tempArray
+    } else {
+        return recursiveRemove(tempArray, value)
+    }
+}
+
+const removeFromArray = function(array, ...value) {
+    const tempArray = [...array]
+    value.forEach(item => {
+        recursiveRemove(tempArray, item)
+    })
+    return tempArray
 };
 
 // Do not edit below this line
