81120528a302993a8380ef6e19d0bc0f698b8325
ijuntan <juniortanaya@gmail.com>
2024-07-31 22:19:20 +0800

    feat: add fibonacci feature

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS"
+    num = Number(num)
+    if(num === 0) return 0
+    if(num === 1 || num === 2) return 1
+    let prevSum = 1;
+    let sum = 1;
+    for(i = 2 ; i < num; i++) {
+        let temp = prevSum;
+        prevSum = sum;
+        sum += temp;
+    }
+    return sum
 };
 
 // Do not edit below this line
