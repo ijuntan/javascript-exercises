15ea5cb26808b97a7be29f9f98ab8a1ea027747a
ijuntan <juniortanaya@gmail.com>
2024-07-31 22:38:08 +0800

    feat: add finding the name of oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, cur) => {
+        const age = (cur.yearOfDeath || 2024) - cur.yearOfBirth
+        
+        if(age > acc.age)
+            return {
+                name: cur.name,
+                age
+            }
+        return acc
+    }, {name: '', age: 0})
 };
 
 // Do not edit below this line
